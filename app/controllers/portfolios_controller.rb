06eb556e7800c9536ec29de43b60bb2f912daa9f
Alejandro Alemán <alejandro.aleman@gmail.com>
2018-08-22 19:57:03 +0200

    Implemented edit and update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,24 +8,42 @@ def new
   @portfolio = Portfolio.new
 end
 
-  # POST /portfolio
-  # POST /portfolio.json
-  def create
-    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+# POST /portfolio
+# POST /portfolio.json
+def create
+  @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
-      if @portfolio.save
-        #format.html { redirect_to @portfolio, notice: 'Your portfolio item is now live.' }
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-      end
+  respond_to do |format|
+    if @portfolio.save
+      #format.html { redirect_to @portfolio, notice: 'Your portfolio item is now live.' }
+      format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      format.json { render :show, status: :created, location: @portfolio }
+    else
+      format.html { render :new }
+      format.json { render json: @portfolio.errors, status: :unprocessable_entity }
     end
   end
+end
+
+def edit
+  @portfolio = Portfolio.find(params[:id])
+end
 
 
+# PATCH/PUT /portfolios/1
+# PATCH/PUT /posrtfolios/1.json
+def update
+  @portfolio = Portfolio.find(params[:id])
+  respond_to do |format|
+    if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      format.json { render :show, status: :ok, location: @portfolio }
+    else
+      format.html { render :edit }
+      format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+    end
+  end
+end
 
 
 
