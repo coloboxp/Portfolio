d3db0a2ff6847d66334200574a8748c578bba7ad
Alejandro Alemán <alejandro.aleman@gmail.com>
2018-08-22 21:22:19 +0200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/learning'
-  get 'pages/interest'
+  #get 'pages/home'
+  get 'about', to: "pages#about"
+  get 'contact', to: "pages#contact"
+  get 'learning', to: "pages#learning"
+  get 'interest', to: "pages#interest"
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#home"
 end
