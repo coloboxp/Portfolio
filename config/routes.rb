f59a169992691cd15194fcacf63b19c12b8e410d
Alejandro Alemán <alejandro.aleman@gmail.com>
2018-08-23 14:06:11 +0200

    Added special route for action toggle_status, modified blog index view

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,14 @@ Rails.application.routes.draw do
   get 'contact', to: "pages#contact"
   get 'learning', to: "pages#learning"
   get 'interest', to: "pages#interest"
-  resources :blogs
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
