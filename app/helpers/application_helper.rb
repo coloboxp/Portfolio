dcf2c865a06f970dc88b9186877e0a50fdbc7f6a
Alejandro Alemán <alejandro.aleman@gmail.com>
2018-09-01 12:30:39 +0200

    Implemented a view helper, for auth links, to refactor the code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def sample_helper_html
+    #This will render the string as HTML
+    "<p>Hi there!</p>".html_safe
+  end
+
+  def sample_helper_string
+    #This will cleanup or sanitize the string
+    # to strop out the HTML
+    "<p>Hi there!</p>"
+    #in other words, the htm will be shown as string
+    # and this is good so we don't execute some malicious
+    # code by mistake, ie, if an user possts crafted HTML
+  end
+
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout #{current_user.first_name}...", destroy_user_session_path, method: :delete
+    else
+      (link_to "Sign in", new_user_session_path, method: :get) +
+      "<br>".html_safe +
+      (link_to "Register", new_user_registration_path, method: :get)
+    end
+  end
 end
